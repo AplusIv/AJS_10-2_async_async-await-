1999e1254035bcfeab2e774db7fe5c24b7138522
aplusiv <50slash50@gmail.com>
2022-05-31 14:44:02 +0300

    first commit

---
+++ src/js/gameSavingLoader.js
@@ -0,0 +1,20 @@
+import json from './parser';
+import read from './reader';
+
+export default class GameSavingLoader {
+  static load() {
+    (async () => {
+      try {
+        const data = await new Promise((resolve) => resolve(read()));
+        const resultStr = await new Promise((resolve) => resolve(json(data)));
+        const result = JSON.parse(resultStr);
+        // console.log(JSON.parse(resultStr));
+        return result; // ??? Здесь промис возвращается?
+        // Ожидал объект в консоли, а там ничего не выводится
+        // (просто пусто, будто ничего не отработало)
+      } catch (err) {
+        throw new Error(err);
+      }
+    })(); // ??? За что отвечают скобочки "()" в конце строки? (async ()=>{...})()
+  }
+}
